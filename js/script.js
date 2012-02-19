4c505d7c5571ff0d57e4ac1d5bc32a480c251c7a
Darren Day <darrend@missiondata.com>
2012-02-18 22:57:29 -0500

    pulled in jsfiddle experiment

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-/* Author:
+/* Author: Darren Day
 
 */
+function handlePinboardResponse(data) {
+    var source = $("#template").html();
+    var template = Handlebars.compile(source);
+    var result = template({
+        "links": data
+    });
+    $("#main").append(result);
+}
+
+function refreshFromPinboard() {
+    $.ajax({
+        url: "http://feeds.pinboard.in/json/v1/u:thurston/t:start?format=json",
+        dataType: 'jsonp',
+        jsonp: 'cb',
+        success: handlePinboardResponse
+    });
+}
+
+$(document).ready(function() {
+    refreshFromPinboard();
+});
 
 
 
